985a3ea7bd93526417cccfa4c7a5cc0278db310f
Sergio Gonzalez <bigmonachus@gmail.com>
2018-06-04 22:25:21 -0700

    WIP Windows. Read pipe from child process.

--- platform_windows.c
+++ platform_windows.c
@@ -54,27 +54,87 @@ winPrintError(int err_code) {
 
 int
 platformCreateProcess(char* name, char** args, sz n_args) {
-    STARTUPINFO startup_info = Zero;
-    PROCESS_INFORMATION proc_info = Zero;
+   HANDLE stdin_write = 0;
+   HANDLE stdin_read = 0;
+   HANDLE stdout_write = 0;
+   HANDLE stdout_read = 0;
 
-    // TODO: UTF-16 path names
-    BOOL cpr = CreateProcess(/*lpApplicationName*/ name,
-                             /*lpCommandLine*/ NULL,
-                             /*lpProcessAttributes*/ NULL,
-                             /*lpThreadAttributes*/ NULL,
-                             /*bInheritHandles*/ FALSE,
-                             /*dwCreationFlags*/0,
-                             /*lpEnvironment*/NULL,
-                             /*lpCurrentDirectory*/NULL,
-                             /*lpStartupInfo*/&startup_info,
-                             /*lpProcessInformation*/&proc_info);
+   STARTUPINFO startup_info = {
+      .cb = sizeof(STARTUPINFO),
+      .hStdError = stdout_write,
+      .hStdOutput = stdout_write,
+      .hStdInput = stdin_read,
+      .dwFlags = STARTF_USESTDHANDLES,
+   };
+   SECURITY_ATTRIBUTES security_attrs = {
+      .nLength = sizeof(SECURITY_ATTRIBUTES),
+      .bInheritHandle = TRUE,
+      .lpSecurityDescriptor = NULL,
+   };
+   PROCESS_INFORMATION proc_info = Zero;
 
-    if (!cpr) {
-        int err = GetLastError();
-        winPrintError(err);
-        fprintf(stderr, "CreateProcess failed: Error %d", err);
-        return 1;
-    }
+   if (!CreatePipe(&stdout_read, &stdout_write, &security_attrs, 0) ) {
+      winPrintError(GetLastError());
+   }
+   else if (!CreatePipe(&stdin_read, &stdin_write, &security_attrs, 0)) {
+      winPrintError(GetLastError());
+   }
+#if 0
+   else if (!SetHandleInformation(stdout_read, HANDLE_FLAG_INHERIT, (1<<31) - 1) ) {
+      winPrintError(GetLastError());
+   }
+   else if (!SetHandleInformation(stdin_write, HANDLE_FLAG_INHERIT, (1<<31) - 1) ) {
+      winPrintError(GetLastError());
+   }
+#endif
+   // Create the child process.
+   else {
+      BOOL cpr = CreateProcess(/*lpApplicationName*/ name,
+                               /*lpCommandLine*/ NULL,
+                               /*lpProcessAttributes*/ NULL,
+                               /*lpThreadAttributes*/ NULL,
+                               /*bInheritHandles*/TRUE,
+                               /*dwCreationFlags*/0,
+                               /*lpEnvironment*/NULL,
+                               /*lpCurrentDirectory*/NULL,
+                               /*lpStartupInfo*/&startup_info,
+                               /*lpProcessInformation*/&proc_info);
 
-    return 0;
+      if (!cpr) {
+         int err = GetLastError();
+         winPrintError(err);
+         fprintf(stderr, "CreateProcess failed: Error %d", err);
+         return 1;
+      }
+      else {
+         {
+            // Read pipe
+            DWORD read_bytes, dwWritten;
+            CHAR buffer[1] = Zero;
+            BOOL ok = FALSE;
+            HANDLE hParentStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+            for (;;) {
+               // TODO: Why is this blocking?
+               ok = ReadFile(stdout_read, buffer, ArrayCount(buffer), &read_bytes, NULL);
+               if(!ok)  {
+                  winPrintError(GetLastError());
+                  break;
+               }
+               if (read_bytes == 0) break;
+            }
+         }
+         DWORD exit_code = 0;
+         GetExitCodeProcess(proc_info.hProcess, &exit_code);
+         if (exit_code != 0) {
+            fprintf(stderr, "NASM failed with error code %ld\n", exit_code);
+
+         }
+         else {
+         }
+         CloseHandle(proc_info.hProcess);
+         CloseHandle(proc_info.hThread);
+      }
+   }
+   return 0;
 }
